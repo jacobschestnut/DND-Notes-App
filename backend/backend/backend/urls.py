bb32e67ec85baa90653a51d78d05c119f6afdde5
Jacob Chestnut <jacob.chestnut@fbitn.com>
2025-05-16 14:47:38 -0500

    inital commit

---
+++ backend/backend/backend/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path, include
+from api.views import CreateUserView, NoteDelete, NoteListCreate
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+urlpatterns = [
+    path('notes/', NoteListCreate.as_view(), name='note-list-create'),
+    path('notes/<int:pk>/', NoteDelete.as_view(), name='note-delete'),
+    path('register/', CreateUserView.as_view(), name='user-register'),
+    path('admin/', admin.site.urls),
+    path("api/user/register/", CreateUserView.as_view(), name="register"),
+    path("api/token/", TokenObtainPairView.as_view(), name="get_token"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="refresh"),
+    path("api-auth", include("rest_framework.urls")),
+]
